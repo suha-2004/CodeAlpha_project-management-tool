ed0cb2400493fd5f8ef182dcc7b109ce29d0400b
suha <suhaia2004@gmail.com>
2026-09-05 14:55:55 +0400

    Fix project model filename case

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Project = require("../models/Project");
+const Project = require("../models/project");
 const jwt = require("jsonwebtoken");
 
 // ================= AUTH MIDDLEWARE =================
